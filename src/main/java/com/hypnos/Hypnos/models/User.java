c4ec324062b22a938a749661eac81ead84e14fdf
ALVARO LAHOZ <alvarolahozmontero24@gmail.com>
2024-05-21 20:19:03 +0200

    Añadidos role ADMIN a nosotros en el data.sql

--- src/main/java/com/hypnos/Hypnos/models/User.java
+++ src/main/java/com/hypnos/Hypnos/models/User.java
@@ -65,7 +65,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return role.getAuthorities();
     }
 
     @Override
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO users (email, alias, password)
-VALUES ('user','alvaro_lahoz','$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPO'); //password - codificado con bcrypt
+INSERT INTO users (email, alias, password,role)
+VALUES ('alvarolahozmontero@gmail.com','alvaro_lahoz','$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPL','ADMIN'),
+       ('anabatrescuellar@gmail.com','ana_batres','$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPA','ADMIN'); //password - codificado con bcrypt
