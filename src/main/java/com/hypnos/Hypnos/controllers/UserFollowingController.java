a739909ef39854828f3dc63db095c19ff3e34793
Ana Batres <batres.anaa@gmail.com>
2024-06-10 14:54:04 +0200

    Arreglado UserFollowingController

--- src/main/java/com/hypnos/Hypnos/controllers/UserFollowingController.java
+++ src/main/java/com/hypnos/Hypnos/controllers/UserFollowingController.java
@@ -3,6 +3,7 @@ package com.hypnos.Hypnos.controllers;
 import com.hypnos.Hypnos.models.User;
 import com.hypnos.Hypnos.models.UserFollowing;
 import com.hypnos.Hypnos.services.user.UserFollowingService;
+import com.hypnos.Hypnos.services.user.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,34 +18,32 @@ public class UserFollowingController {
     @Autowired
     private UserFollowingService userFollowingService;
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @PostMapping("/{followerId}/{followedId}")
     public UserFollowing followUser(@PathVariable Long followerId, @PathVariable Long followedId) {
-        User follower = new User(); // Debes obtener el usuario real desde tu servicio de usuarios
-        follower.setId(followerId);
-        User followed = new User(); // Debes obtener el usuario real desde tu servicio de usuarios
-        followed.setId(followedId);
+        User follower = userService.findById(followerId);
+        User followed = userService.findById(followedId);
         return userFollowingService.followUser(follower, followed);
     }
-
     @DeleteMapping("/{followerId}/{followedId}")
     public void unfollowUser(@PathVariable Long followerId, @PathVariable Long followedId) {
-        User follower = new User(); // Debes obtener el usuario real desde tu servicio de usuarios
-        follower.setId(followerId);
-        User followed = new User(); // Debes obtener el usuario real desde tu servicio de usuarios
-        followed.setId(followedId);
+        User follower = userService.findById(followerId);
+        User followed = userService.findById(followedId);
         userFollowingService.unfollowUser(follower, followed);
     }
 
     @GetMapping("/followers/{userId}")
     public List<UserFollowing> getFollowers(@PathVariable Long userId) {
-        User user = new User(); // Debes obtener el usuario real desde tu servicio de usuarios
+        User user = new User();
         user.setId(userId);
         return userFollowingService.getFollowers(user);
     }
 
     @GetMapping("/following/{userId}")
     public List<UserFollowing> getFollowedUsers(@PathVariable Long userId) {
-        User user = new User(); // Debes obtener el usuario real desde tu servicio de usuarios
+        User user = new User();
         user.setId(userId);
         return userFollowingService.getFollowedUsers(user);
     }
